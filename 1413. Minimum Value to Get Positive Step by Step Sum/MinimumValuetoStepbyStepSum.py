eb77a55f937966207dec9ce0ae98368758efd03f
Chenbo Yang <74288362+ChenboYang97@users.noreply.github.com>
2021-11-13 20:11:43 -0800

    Update MinimumValuetoStepbyStepSum.py

--- 1413. Minimum Value to Get Positive Step by Step Sum/MinimumValuetoStepbyStepSum.py
+++ 1413. Minimum Value to Get Positive Step by Step Sum/MinimumValuetoStepbyStepSum.py
@@ -1,20 +1,22 @@
 class Solution:
-    def maxProfit(self, prices: list[int]) -> int:
-        min_price = prices[0]
-        profit = 0
-        max_profit = 0
-
-        for stock_price in prices:
-            if stock_price < min_price:
-                min_price = stock_price
-            profit = stock_price - min_price
-            if profit > max_profit:
-                max_profit = profit
-
-        return max_profit
+    def minStartValue(self, nums: list[int]) -> int:        
+        init_start = 1
+        start = 1
+        index = 0
+        
+        while (index < len(nums)):
+            start += nums[index]
+            if start < 1:
+                init_start += 1 - start
+                start = 1
+            index += 1
+            
+        return init_start
 
 if __name__ == '__main__':
-    prices = [7,1,5,3,6,4]
-    assert(Solution().maxProfit(prices) == 5)
-    prices = [7,6,4,3,1]
-    assert(Solution().maxProfit(prices) == 0)
+    nums = [-3,2,-3,4,2]
+    assert(Solution().minStartValue(nums) == 5)
+    nums = [1,2]
+    assert(Solution().minStartValue(nums) == 1)
+    nums = [1,-2,-3]
+    assert(Solution().minStartValue(nums) == 5)
